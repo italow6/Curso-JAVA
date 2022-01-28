2e707b5d385fe827c7f20ac575096a3c77e9ceaa
Ítalo Iran <italow6@gmail.com>
2022-01-28 07:43:33 -0300

    Tipos de dados e variáveis

---
+++ src/Variavel.java
@@ -0,0 +1,15 @@
+
+public class Variavel {
+	public static void main (String args[]) {
+		short valor1 = 15;
+		short valor2 = 25;
+		
+		short valor3 = (short) (valor1 + valor2);
+		
+		
+		System.out.println(valor1);
+		System.out.println(valor2);
+		System.out.println(valor3);
+		
+	}
+}
